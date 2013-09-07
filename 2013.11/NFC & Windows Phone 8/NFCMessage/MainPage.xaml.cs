9de108389bfe31120a0f03a0c43a40310329b434
Matthias Fischer <matthias@dotnetautor.de>
2013-09-07 17:13:41 +0200

    Windows Phone 8 NFC Beispiele hinzugefügt

---
+++ 2013.11/NFC & Windows Phone 8/NFCMessage/MainPage.xaml.cs
@@ -0,0 +1,219 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Windows;
+using Microsoft.Phone.Controls;
+using Windows.Networking.Proximity;
+using Windows.Storage.Streams;
+
+namespace NFCMessage {
+  public partial class MainPage : PhoneApplicationPage {
+
+    private long _publishedMessageId = -1;
+    private long _subscriptionId = -1;
+
+    private long _idUriWrite = -1;
+    private long _idWinMime;
+    private long _idTxtPlain;
+    private long _launchAppPubId = -1;
+
+    // Constructor
+    public MainPage() {
+      InitializeComponent();
+      CheckForNfc();
+    }
+
+
+    private void CheckForNfc() {
+      ProximityDevice device = ProximityDevice.GetDefault();
+      if (device != null) {
+
+        MessageBox.Show("NFC steht zur Verfügung");
+        device.DeviceArrived += sender => {
+            // Gerät in reichweite
+          };
+
+        device.DeviceDeparted += sender => {
+            // Gerät hat reichweite verlassen
+          };
+      } else {
+        MessageBox.Show(
+          "Ihr Telefon hat entweder kein NFC oder NFC ist deaktiviert");
+      }
+    }
+
+    private void btnPublish_Click(object sender, RoutedEventArgs e) {
+
+      ProximityDevice device = ProximityDevice.GetDefault();
+
+      // Prüfen ob NFC auf diesem Gerät verfügbar ist
+      if (device != null) {
+        if (_publishedMessageId > -1) device.StopPublishingMessage(_publishedMessageId);
+        _publishedMessageId = device.PublishMessage("Windows.WindowsDeveloper.MessageType", "Hallo Welt via NFC");
+      }
+
+    }
+
+    private void btnStopPublic_Click(object sender, RoutedEventArgs e) {
+      ProximityDevice device = ProximityDevice.GetDefault();
+
+      // Prüfen ob NFC auf diesem Gerät verfügbar ist
+      if (device != null && _publishedMessageId > -1) {
+        device.StopPublishingMessage(_publishedMessageId);
+        _publishedMessageId = -1;
+      }
+    }
+
+    private void btnSubscribe_Click(object sender, RoutedEventArgs e) {
+      ProximityDevice device = ProximityDevice.GetDefault();
+
+      // Prüfen ob NFC auf diesem Gerät verfügbar ist
+      if (device != null) {
+        if (_subscriptionId > -1) device.StopSubscribingForMessage(_subscriptionId);
+        _subscriptionId = device.SubscribeForMessage(
+          "Windows.WindowsDeveloper.MessageType",
+          (proximityDevice, message) => Dispatcher.BeginInvoke(() =>
+                                                               MessageBox.Show(
+                                                                 "Nachricht empfangen " +
+                                                                 message.DataAsString +
+                                                                 " von " +
+                                                                 proximityDevice.DeviceId)
+                                          ));
+      }
+    }
+
+    private void btnUnsubscribe_Click(object sender, RoutedEventArgs e) {
+      ProximityDevice device = ProximityDevice.GetDefault();
+
+      // Prüfen ob NFC auf diesem Gerät verfügbar ist
+      if (device != null && _subscriptionId > -1) {
+        device.StopSubscribingForMessage(_subscriptionId);
+      }
+    }
+
+    private void btnWriteURI_Click(object sender, RoutedEventArgs e) {
+     ProximityDevice device = ProximityDevice.GetDefault();
+
+      if (device != null) {
+        _idUriWrite = device.PublishBinaryMessage(
+          "WindowsUri:WriteTag",
+          Encoding.Unicode.GetBytes(tbUrl.Text).AsBuffer(),
+          (proximityDevice, messageId) =>
+            {
+              Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                  device.StopPublishingMessage(_idUriWrite);
+                  MessageBox.Show("Nachricht erfolgreich geschrieben !");
+                });
+            }
+          );
+      }
+    }
+
+    private void btnWriteLauch_Click(object sender, RoutedEventArgs e) {
+     ProximityDevice device = ProximityDevice.GetDefault();
+
+      if (device != null) {
+        //var appId = Windows.ApplicationModel.Store.CurrentApp.AppId;
+        var appId = "4bad5210-10de-4ae1-a10d-3bb5ce218d66";
+        const string launchArgs = "user=default";
+
+        string appName = "NFCMessage" + "!" + appId;
+
+        string launchAppMessage = launchArgs + "\tWindows\t" + appName;
+
+        var dataWriter = new DataWriter {
+          UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf16LE
+        };
+
+        dataWriter.WriteString(launchAppMessage);
+        _launchAppPubId = device.PublishBinaryMessage(
+          "LaunchApp:WriteTag", dataWriter.DetachBuffer(), (proximityDevice, id) =>
+                                                           Deployment.Current.Dispatcher.BeginInvoke(() =>
+                                                             {
+                                                               device.StopPublishingMessage(_launchAppPubId);
+                                                               MessageBox.Show("Nachricht erfolgreich geschrieben !");
+                                                             }));
+
+      }
+    }
+
+    private void btnSubscribeWritable_Click(object sender, RoutedEventArgs e) {
+      
+        ProximityDevice device = ProximityDevice.GetDefault();
+
+        // Prüfen ob NFC auf diesem Gerät verfügbar ist
+        if (device != null) {
+          if (_subscriptionId > -1) device.StopSubscribingForMessage(_subscriptionId);
+          _subscriptionId = device.SubscribeForMessage("WriteableTag", (proximityDevice, message) => {
+            var size = System.BitConverter.ToInt32(message.Data.ToArray(), 0);
+            Deployment.Current.Dispatcher.BeginInvoke(() => {
+              MessageBox.Show(string.Format("Beschreibbarer Tag, Größe:{0}Bytes", size));
+            });
+          });
+        }
+      }
+
+
+    private void btnWinMime_Click(object sender, RoutedEventArgs e) {
+      ProximityDevice device = ProximityDevice.GetDefault();
+
+      if (device != null) {
+        if (_subscriptionId > -1) device.StopSubscribingForMessage(_subscriptionId);
+        _subscriptionId = device.SubscribeForMessage(
+          "WindowsMime",
+          (proximityDevice, message) =>
+            {
+              var buffer = message.Data.ToArray();
+              int mimesize = 0;
+              //suchen nach '\0' 
+              for (mimesize = 0; mimesize < 256 && buffer[mimesize] != 0; ++mimesize) {}
+
+              //MimeType bestimmen
+              var mimeType = Encoding.UTF8.GetString(buffer, 0, mimesize).Trim();
+              if (mimeType == "text/plain") {
+                //convert data to string. This traitement depend on mimetype value.
+                tbMime.Text = Encoding.UTF8.GetString(buffer, 256, buffer.Length - 256);
+              }
+            });
+      }
+    }
+
+    private void btnWinMimePublish_Click(object sender, RoutedEventArgs e) {
+      ProximityDevice device = ProximityDevice.GetDefault();
+
+      if (device != null) {
+        btnWinMimePublish.IsEnabled = false;
+         _idWinMime = device.PublishBinaryMessage(
+          "WindowsMime.text/plain",
+          Encoding.UTF8.GetBytes(tbMime.Text).AsBuffer(),
+          (proximityDevice, messageId) => {
+            Deployment.Current.Dispatcher.BeginInvoke(() => {
+              device.StopPublishingMessage(_idWinMime);
+                MessageBox.Show("Nachricht erfolgreich geschrieben !");
+                btnWinMimePublish.IsEnabled = true;
+              });
+          }
+          );
+      }
+    }
+
+    private void btnWriteMimePublish_Click(object sender, RoutedEventArgs e) {
+      ProximityDevice device = ProximityDevice.GetDefault();
+
+      if (device != null) {
+        btnWriteMimePublish.IsEnabled = false;
+        _idTxtPlain = device.PublishBinaryMessage(
+          "WindowsMime:WriteTag.text/plain",
+          Encoding.UTF8.GetBytes(tbMime.Text).AsBuffer(),
+          (proximityDevice, messageId) => {
+            Deployment.Current.Dispatcher.BeginInvoke(() => {
+              device.StopPublishingMessage(_idTxtPlain);
+                MessageBox.Show("Nachricht erfolgreich geschrieben !");
+                btnWriteMimePublish.IsEnabled = true;
+              });
+          }
+          );
+      }
+    }
+  }
+}
---
+++ 2013.11/NFC & Windows Phone 8/NFCSimpleChat/MainPage.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Windows.Networking.Proximity;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace NFCSimpleChat {
+  public partial class MainPage : PhoneApplicationPage {
+
+    private StreamSocket _proximitySocket = null;
+    private PeerInformation _requestingPeer;
+    private DataReader _dataReader;
+    private DataWriter _dataWriter;
+    private bool _started = false;
+
+    // Constructor
+    public MainPage() {
+      InitializeComponent();
+
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e) {
+      DisplayNameTextBox.Text = PeerFinder.DisplayName;
+      // /MainPage.xaml?ms_nfp_launchargs=Windows.Networking.Proximity.PeerFinder:StreamSocket
+
+      // If activated from launch or from the background, create a peer connection.
+      if (e.Uri.ToString().IndexOf("ms_nfp_launchargs=Windows.Networking.Proximity.PeerFinder:StreamSocket", StringComparison.InvariantCultureIgnoreCase) > -1) {
+        AdvertiseForPeersButton_Click(null, null);
+      }
+
+    }
+
+    protected override void OnNavigatingFrom(NavigatingCancelEventArgs e) {
+      if (_started) {
+        // Detach the callback handler (there can only be one PeerConnectProgress handler).
+        PeerFinder.TriggeredConnectionStateChanged -= TriggeredConnectionStateChanged;
+        // Detach the incoming connection request event handler.
+        PeerFinder.Stop();
+        CloseSocket();
+        _started = false;
+      }
+    }
+
+
+     private void WriteMessageText(string message, bool overwrite = false) {
+       Dispatcher.BeginInvoke(() => {
+            if (overwrite)
+              tbResult.Text = message;
+            else
+              tbResult.Text += message;
+          });
+    }
+
+    
+
+    // Click event handler for "Advertise" button.
+    private void AdvertiseForPeersButton_Click(object sender, RoutedEventArgs e) {
+      if (_started) {
+        WriteMessageText("Sie warten bereits auf eine Verbindung !\r\n");
+        return;
+      }
+
+      PeerFinder.DisplayName = DisplayNameTextBox.Text;
+
+      if ((PeerFinder.SupportedDiscoveryTypes & PeerDiscoveryTypes.Triggered) == PeerDiscoveryTypes.Triggered) {
+        PeerFinder.TriggeredConnectionStateChanged += TriggeredConnectionStateChanged;
+
+        WriteMessageText("Tappen Sie ein Gerät an, das auch auf eine Verbindung wartet.\n");
+      } else {
+        WriteMessageText("Antippen für eine Verbindung wird von Ihrem Gerät nicht unterstützt.\n");
+      }
+
+      if ((PeerFinder.SupportedDiscoveryTypes & PeerDiscoveryTypes.Browse) != PeerDiscoveryTypes.Browse) {
+        WriteMessageText("Das Verbinden über W-Lan wird von Ihrem Gerät nicht unterstützt.\n");
+      }
+
+      PeerFinder.Start();
+      _started = true;
+    }
+
+    private void TriggeredConnectionStateChanged(
+        object sender,
+        TriggeredConnectionStateChangedEventArgs e) {
+      if (e.State == TriggeredConnectState.PeerFound) {
+        WriteMessageText("Gerät in Reichweite gefunden.\n");
+      }
+      if (e.State == TriggeredConnectState.Completed) {
+        Connected(e.Socket);
+      }
+    }
+
+    private void Connected(StreamSocket socket) {
+      // DataReader und Writer erstelen
+      _dataReader = new DataReader(socket.InputStream);
+      _dataWriter = new DataWriter(socket.OutputStream);
+      WriteMessageText("Verbunden\r\n", true);
+      ListenForIncomingMessage();
+    }
+
+    private async void ListenForIncomingMessage() {
+      try {
+        // Länge und Nachricht lesen
+        uint sizeFieldCount = await _dataReader.LoadAsync(1);
+        byte b = _dataReader.ReadByte();
+        await _dataReader.LoadAsync(b);
+         WriteMessageText( "\r\n<--" + _dataReader.ReadString(b));
+
+        // Auf die nächste nachricht warten.
+       ListenForIncomingMessage();
+      } catch (Exception ex) {
+        Dispatcher.BeginInvoke(() => MessageBox.Show("Verbindung unterbrochen oder beendet!"));
+        CloseSocket();
+      }
+    }
+
+    // Click event handler for "Stop" button.
+    private void StopFindingPeersButton_Click(object sender, RoutedEventArgs e) {
+      _started = false;
+      PeerFinder.Stop();
+      CloseSocket(); 
+    }
+
+    // Handle external connection requests.
+
+
+    private async void btnSend_Click(object sender, RoutedEventArgs e) {
+
+      // Länge der Nachricht
+      uint commandSize = _dataWriter.MeasureString(tbSend.Text);
+      _dataWriter.WriteByte((byte)commandSize);
+
+      // Nachricht
+      var sentCommandSize = _dataWriter.WriteString(tbSend.Text);
+      await _dataWriter.StoreAsync();
+
+       WriteMessageText("\r\n-->" + tbSend.Text);
+      tbSend.Text = "";
+
+    }
+
+    private void CloseSocket() {
+      if (_dataReader != null) {
+        _dataReader.Dispose();
+        _dataReader = null;
+      }
+
+      if (_dataWriter != null) {
+        _dataWriter.Dispose();
+        _dataWriter = null;
+      }
+
+      if (_proximitySocket != null) {
+        _proximitySocket.Dispose();
+        _proximitySocket = null;
+      }
+    }
+
+  }
+}
